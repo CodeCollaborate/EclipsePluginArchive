a2be917247563ec68471f9d0b4bbe06d519f0105
Feefity <feefity@gmail.com>
2015-11-05 18:58:50 -0500

    Fixed PatchHandler to handle received patches correctly

--- CodeCollaborateEclipse/src/codecollaborateeclipse/document/PatchHandler.java
+++ CodeCollaborateEclipse/src/codecollaborateeclipse/document/PatchHandler.java
@@ -44,55 +44,58 @@ public class PatchHandler {
 	public Object[] recievePatch(String message, String text) {
 		
 		Object[] results = new Object[4];
-		//parse JSON data
+		
 		try {
+			
 			System.out.println(message);
-			//deal with patch text
+			
+			//deal with patch text and apply patch
 			Patch p = dmp.patch_fromText(message).get(0);
 			LinkedList<Patch> patches = new LinkedList<Patch>();
+			patches.add(p);
 			Object[] appliedPatchResults = dmp.patch_apply(patches, text);
-			//TODO: check boolean array for fuck-ups
 			
-			PatchRange[] ranges = (PatchRange[]) appliedPatchResults[2];
-			String prefix;
-			String operation;
-			String suffix;
-			if (p.diffs.size() == 1) {
-				prefix = "";
-				operation = p.diffs.getFirst().operation.toString();
-				suffix = p.diffs.getLast().text;
-			} else if (!p.diffs.getFirst().operation.toString().equals("EQUAL")) {
-				prefix = "";
-				operation = p.diffs.getFirst().operation.toString();
-				suffix = p.diffs.getLast().text;
-			} else if (!p.diffs.getLast().operation.toString().equals("EQUAL")) {
-				prefix = p.diffs.getFirst().text;
-				operation = p.diffs.get(1).operation.toString();
-				suffix = p.diffs.getLast().text;
-			} else {
-				prefix = p.diffs.getFirst().text;
-				operation = p.diffs.get(1).operation.toString();
-				suffix = p.diffs.getLast().text;
+			//check boolean array for fuck-ups
+			boolean[] applied = (boolean[]) appliedPatchResults[1];
+			for (boolean b : applied) {
+				if (!b) {
+					System.out.println("Oopsie! Your patch didn't apply!");
+					throw new Exception();
+				}
 			}
-			System.out.println("Change to be made: " + suffix);
-			System.out.println("Operation: " + operation);
-			System.out.println("Context: " + prefix);
+			
+			//pull out patch ranges
+			PatchRange[] ranges = (PatchRange[]) appliedPatchResults[2];
+			
+			//deal with Diff cases
+			String operation;
+			String edit;
+			
+			if (p.diffs.size() == 1) {
+				operation = p.diffs.getFirst().operation.toString();
+				edit = p.diffs.getFirst().text;
+			} else if (p.diffs.size() == 2) {
+				if (p.diffs.getFirst().operation.toString().equals("EQUAL")) {
+					operation = p.diffs.getLast().operation.toString();
+					edit = p.diffs.getLast().text;
+				} else {
+					operation = p.diffs.getFirst().operation.toString();
+					edit = p.diffs.getFirst().text;
+				}
+			} else {
+				operation = p.diffs.get(1).operation.toString();
+				edit = p.diffs.get(1).text;
+			}
+			
+			//some debug print statements
+//			System.out.println("Change to be made: " + edit);
+//			System.out.println("Operation: " + operation);
+//			System.out.println("Context: " + prefix);
 //			int insertPos = dmp.match_main(text, prefix, p.start1);
 			int start = ranges[0].getStartPos();
 			int end = ranges[0].getEndPos();
 			
-//			LinkedList<Patch> patchList = new LinkedList<Patch>();
-//			patchList.add(p);
-//			Object[] patchResult = dmp.patch_apply(patchList, text);
-//			
-//			boolean[] patchesApplied = (boolean[]) patchResult[1];
-//			
-//			for(boolean patchAppliedResult : patchesApplied) {
-//				if(!patchAppliedResult) {
-//					System.out.println("Failed to apply a patch");
-//				}
-//			}
-			results[0] = suffix;
+			results[0] = edit;
 			results[1] = operation;
 			results[2] = start;
 			results[3] = end;
